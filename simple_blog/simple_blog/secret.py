d634b8143ca616e522ef3f3c7008779725b79c9f
doubi3 <mark.ekperi@gmail.com>
2021-04-25 12:18:06 +0100

    Configured secret

---
+++ simple_blog/simple_blog/secret.py
@@ -0,0 +1,1 @@
+Secret_Key = 'django-insecure-jk6hst7xmigmpzt88*c#1so^(c#p2sg8(u_e**+^%@9ki)+2$p'
--- simple_blog/simple_blog/settings.py
+++ simple_blog/simple_blog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from secret import Secret_Key
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jk6hst7xmigmpzt88*c#1so^(c#p2sg8(u_e**+^%@9ki)+2$p'
+SECRET_KEY = Secret_Key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #My Simple blog app
+    # My Simple blog app
     'blog'
 ]
 
